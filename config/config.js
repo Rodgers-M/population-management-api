e7496e6359489871f04fb971b006245bb9979d2d
Rodgers Kishagha <mkishagha@gmail.com>
2019-03-17 22:43:27 +0300

    setup application server

---
+++ config/config.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const joi = require('joi')
+
+const envVarsSchema = joi.object({
+  NODE_ENV: joi.string()
+    .allow(['development', 'production', 'test', 'staging'])
+    .required(),
+  PORT: joi.number().default(8080),
+  DATABASE: joi.string().required(),
+  TEST_DB: joi.string().required(),
+  DATABASE_DIALECT: joi.string().default('postgres'),
+  DATABASE_PASSWORD: joi.string().default(null),
+  DATABASE_USER: joi.string().required(),
+  HOST: joi.string().required(),
+  DATABASE_URL: joi.string().required(),
+})
+  .unknown()
+  .required()
+
+const { error, value: envVars } = joi.validate(process.env, envVarsSchema)
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`)
+}
+
+
+const config = {
+  env: envVars.NODE_ENV || 'development',
+  port: envVars.PORT,
+  databaseName: envVars.DATABASE,
+  testDbName: envVars.TEST_DB,
+  dbUsername: envVars.DATABASE_USER,
+  databaseDialect: envVars.DATABASE_DIALECT,
+  dbPassword: envVars.password,
+  host: envVars.HOST,
+  databaseUrl: envVars.DATABASE_URL
+};
+
+module.exports = config;
+
