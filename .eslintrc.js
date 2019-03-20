cda9637772b25bd0390dcd63faa2097a2e936ba6
Rodgers Kishagha <mkishagha@gmail.com>
2019-03-20 05:59:28 +0300

    add httpstatus to dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,16 @@
 module.exports = {
     "extends": "airbnb-base"
     "rules": {
+      "prettier/prettier": [
+        "error",
+          {
+            "singleQuote": true, 
+            "trailingComma": "all",
+            "bracketSpacing": true,
+            "semi": false,
+            "parser": "flow"
+        }
+      ],
       "semi": 0
     }
 };
