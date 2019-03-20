b7c185393141b2c613a1974657533920b0a6c8d5
Rodgers Kishagha <mkishagha@gmail.com>
2019-03-20 06:00:37 +0300

    implement creating and listing of locations

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -1,7 +1,30 @@
 const pushid = require('pushid')
+const HttpStatus = require('http-status')
+const validatePayload = require('../lib/validators/validatePayload')
+const { handleSuccess, handleFailure, handleSuccessResult } = require('../lib/helpers/responseHelper')
+const LocationsRepository = require('../lib/locations')
 
 class LocationController {
-  static createLocation(req, res) {
-    const { name, females, males, parentLocation } = req.body
+  static async createLocation(req, res) {
+    try {
+      const { name, females, males, parentLocation } = req.body
+      const errors = validatePayload(req.body)
+      if(Object.keys(errors).length) return handleFailure(
+        res,
+      HttpStatus.UNPROCESSABLE_ENTITY,
+      {message: errors},
+      )
+      if(parentLocation) {
+        const foundLocation = await LocationsRepository.getLocationByName(parentLocation)
+        if(!foundLocation) return handleFailure(
+          res,
+          HttpStatus.NOT_FOUND,
+          {message: 'parentLocation not found, please provide valid location'},
+        )
+      } 
+      await LocationsRepository.createLocation(req.body)
+    } catch(error) {
+      return handleFailure(res, HttpStatus.INTERNAL_SERVER_ERROR, error)
+    }
   }
 }
