a6d49d02074925c612ca56e04e9854d79253097e
Rodgers Kishagha <mkishagha@gmail.com>
2019-03-21 06:43:06 +0300

    retuen json message instead of plain text

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,7 @@ const locationRoutes = require('./locationRoutes')
 
 const routes = app => {
   app.use('/locations', locationRoutes)
-  app.get('/', (req, res) => res.status(HttpStatus.OK).send('welcome to the population management API'))
+  app.get('/', (req, res) => res.status(HttpStatus.OK).json({message: 'welcome to the population management AP'}))
   //any route apart from the above defined
   app.use('*', (req, res) => res.status(HttpStatus.NOT_FOUND).json({message:'path not found, please check and try again'}))
   return app
