83829fb9c44c6a974ad4924e1f3e61dcd41e2fcd
Rodgers Kishagha <mkishagha@gmail.com>
2019-03-21 06:34:32 +0300

    update sequelize to use config object

--- database/models/index.js
+++ database/models/index.js
@@ -8,18 +8,11 @@ const envDbConfig = dbConfig[env]
 const db = {}
 const basename = path.basename(__filename);
 
-let sequelize
-if(process.env.NODE_ENV === 'production') {
-  sequelize = new Sequelize(envDbConfig.use_env_variable,{
-    dialect: 'postgres'
-  })
-} else {
-  sequelize = new Sequelize(envDbConfig.database, envDbConfig.username, envDbConfig.password, {
+const sequelize = new Sequelize(envDbConfig.database, envDbConfig.username, envDbConfig.password, {
     host: envDbConfig.host,
     dialect: envDbConfig.dialect,
     logging: false,
   })
-}
 
 
 fs.readdirSync(__dirname)
